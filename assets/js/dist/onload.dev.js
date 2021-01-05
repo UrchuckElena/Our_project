ff7dceda62d11da4bd0a47106472032f737799a9
UrchuckElena <ya.super.sophiya@yandex.ua>
2021-01-05 21:52:00 +0200

    upload files

---
+++ assets/js/dist/onload.dev.js
@@ -0,0 +1,49 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.like').on('click', function () {
+    $(this).toggleClass('clicked');
+  });
+  $(".menu__link").click(function () {
+    if (!$(this).hasClass("menu__link--active")) {
+      $(".menu__link").removeClass('menu__link--active');
+      $(this).addClass("menu__link--active");
+      $(".showcase__products").hide();
+      $('#tab-' + $(this).data("id")).show();
+    }
+  });
+  /* slider */
+
+  var slider = $('#slider_list').lightSlider({
+    item: 4,
+    slideMove: 4,
+    slideMargin: 20,
+    speed: 2000,
+    controls: false,
+    responsive: [{
+      breakpoint: 1250,
+      settings: {
+        item: 3,
+        slideMove: 3
+      }
+    }, {
+      breakpoint: 900,
+      settings: {
+        item: 2,
+        slideMove: 2
+      }
+    }, {
+      breakpoint: 650,
+      settings: {
+        item: 1,
+        slideMove: 1
+      }
+    }]
+  });
+  $('#previous').on('click', function () {
+    slider.goToPrevSlide();
+  });
+  $('#next').on('click', function () {
+    slider.goToNextSlide();
+  });
+});
