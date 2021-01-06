92d98571ef8f73227a01c1a68e1e9ad9a36fcdb6
UrchuckElena <ya.super.sophiya@yandex.ua>
2021-01-07 00:24:18 +0200

    add new files

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
+    // hamburger 
+    $(".header__hamburger").click(function() {
+        $(".header__hamburger").toggleClass("is-active");
+        $(".mobile-menu, .menu-shadow").toggleClass("opened");
+    });
+
+    $(".menu-shadow").click(function() {
+        $(".header__hamburger").removeClass("is-active");
+        $(".mobile-menu, .menu-shadow").removeClass("opened");
+    });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 50) {
+            $(".header").addClass("scrolled");
+        } else {
+            $(".header").removeClass("scrolled");
+        }
+    });
+
+    // favorites
     $('.like').on('click', function() {
         $(this).toggleClass('clicked');
     });
 
+    // tabs
     $(".menu__link").click(function() {
         if (!$(this).hasClass("menu__link--active")) {
             $(".menu__link").removeClass('menu__link--active');
